7ac122a6c6b5d9bf243f5ae8737d0ca1d377b18b
fsuarez05 <mellifede5@gmail.com>
2026-09-15 23:56:13 -0300

    Agregar app/src/main/java/uy/federico/mispartidos/BootReceiver.java

---
+++ app/src/main/java/uy/federico/mispartidos/BootReceiver.java
@@ -0,0 +1,9 @@
+package uy.federico.mispartidos;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) { AlarmScheduler.scheduleAll(context); }
+}
