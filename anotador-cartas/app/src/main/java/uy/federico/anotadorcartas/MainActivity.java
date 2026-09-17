b5d9ce054c07975e48a371bc93a33c81d5e0a279
fsuarez05 <mellifede5@gmail.com>
2026-09-17 11:04:40 -0300

    Agregar aplicación Android Anotador de Cartas

---
+++ anotador-cartas/app/src/main/java/uy/federico/anotadorcartas/MainActivity.java
@@ -0,0 +1,42 @@
+package uy.federico.anotadorcartas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override protected void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        if (state == null) webView.loadUrl("file:///android_asset/index.html");
+        else webView.restoreState(state);
+    }
+
+    @Override protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
